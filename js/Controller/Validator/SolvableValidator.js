d5868f97a04c937ef1ad2983ae6846222b15e7ec
Benny Sobol <68543899+BennySobol@users.noreply.github.com>
2022-07-04 09:05:40 +0300

    Simplify SolvableValidator class logic

--- js/Controller/Validator/SolvableValidator.js
+++ js/Controller/Validator/SolvableValidator.js
@@ -7,40 +7,35 @@ class SolvableValidator
 
     countInversions(board)
     {
-        let parity = 0;
-        let row = 0;
+        let inversions = 0;
         let blankRow = 0;
 
         for (let i = 0; i < board.values.length; i++)
         {
-            if (i % board.size == 0)
-            {
-                row++;
-            }
             if (board.values[i] == 0)
             {
-                blankRow = row;
+                blankRow = Number.parseInt(i / board.size) + 1;
                 continue;
             }
             for (let j = i + 1; j < board.values.length; j++)
             {
                 if (board.values[i] > board.values[j] && board.values[j] != 0)
                 {
-                    parity++;
+                    inversions++;
                 }
             }
         }
-        return { parity, blankRow };
+        return { inversions, blankRow };
     }
 
     validateBoardSolvable(board)
     {
-        let {parity, blankRow} = this.countInversions(board);
-        console.log(parity, blankRow);
+        let {inversions, blankRow} = this.countInversions(board);
+        console.log(inversions, blankRow);
         if (board.size % 2 == 0)
         {
-            return (parity + blankRow) % 2 == 0;
+            return (inversions + blankRow) % 2 == 0;
         }
-        return parity % 2 == 0;
+        return inversions % 2 == 0;
     }
 }
