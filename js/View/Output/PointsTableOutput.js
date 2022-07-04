56e3798ced9c0eb33a20bd14f9704940fbf070aa
Benny Sobol <68543899+BennySobol@users.noreply.github.com>
2022-07-04 20:00:52 +0300

    Added score board into hiden modal

--- js/View/Output/PointsTableOutput.js
+++ js/View/Output/PointsTableOutput.js
@@ -9,14 +9,21 @@ class PointsTableOutput {
     {
         const leaderboard = document.getElementById('leaderboard');
         leaderboard.innerHTML = `
-        <div class="leaderboard-container">
-            <div class="leaderboard">
-                <div class="head">
-                    <h1>Score Board</h1>
+        <button id="myBtn">Open Score Board</button>
+        <div id="myModal" class="modal">
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <div class="leaderboard-container">
+                    <div class="leaderboard">
+                        <div class="head">
+                            <h1>Score Board</h1>
+                        </div>
+                        <div id="pointsTable"></div>
+                    </div>
                 </div>
-                <div id="pointsTable"></div>
             </div>
         </div>`
+
         const ul = document.createElement('ul');
         document.getElementById('pointsTable').appendChild(ul);
         
@@ -26,10 +33,26 @@ class PointsTableOutput {
         
             li.innerHTML += this._renderEntry(entry);
         });
+
+
+        var modal = document.getElementById("myModal");
+        var btn = document.getElementById("myBtn");
+        var span = document.getElementsByClassName("close")[0];
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
     }
 
     _renderEntry(entry) 
     {
-        return ` Player: ${entry.playerName}, moves: ${entry.numOfMoves}, dificulty: ${entry.boardSize}`;
+        return ` Player: ${entry.playerName}, moves: ${entry.numOfMoves}, dificulty: ${entry.boardSize}, date: ${entry.startDate}`;
     }
 }
