5b83cc2f9685331d09849d38881af5afa3533e33
Benny Sobol <68543899+BennySobol@users.noreply.github.com>
2022-07-04 14:05:31 +0300

    Added BoardSizeInput to get dynamic board size

---
+++ js/View/Inupt/BoardSizeInput.js
@@ -0,0 +1,19 @@
+class BoardSizeInput {
+    
+    constructor()
+    {
+
+    }
+    
+    getBoardSize() 
+    {
+        var min = 2;
+        var max = 10;
+        var size = parseInt(prompt(`Enter board size (${min} - ${max})`, '3'));
+        while (!size || size < min || size > max)
+        {
+            size = parseInt(prompt(`${size} is not a valid size\nEnter board size (${min} - ${max})`, '3'));
+        }
+        return size;
+    }
+}
